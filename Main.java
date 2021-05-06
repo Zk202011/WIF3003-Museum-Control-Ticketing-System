4451e343e8e7d0cc82f95fe7ac6996a41420a3b1
wif170725 <74998926+wif170725@users.noreply.github.com>
2021-05-07 01:15:57 +0800

    Update Main.java
    I add some new feature 
    simple I/O   
    Complete the thread check it la

--- Main.java
+++ Main.java
@@ -1,46 +1,123 @@
 package main;
 
 import java.util.LinkedList;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Scanner;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class Main {
 
-    public static final int START_TICKET_TIME = 480;
-    public static final int START_TIME = 540;
-    public static final int END_TICKET_TIME = 1020;
-    public static final int END_TIME = 1080;
+    public static volatile int START_TICKET_TIME = 480;
+    public static volatile int START_TIME = 540;
+    public static volatile int END_TICKET_TIME = 1020;
+    public static volatile int END_TIME = 1080;
 
-    static AtomicInteger time = new AtomicInteger(START_TICKET_TIME);
+    static volatile int time = START_TICKET_TIME;
     static TicketingSystem ticketingSystem;
     static VisitSystem visitSystem;
     static ExitSystem exitSystem;
     static LinkedList<Ticket> tickets = new LinkedList<>();
-    static LinkedList<Ticket> ticketsEntered = new LinkedList<>();
+    static PriorityBlockingQueue<Ticket> ticketsEntered = new PriorityBlockingQueue<>();
     static LinkedList<Ticket> ticketsLeft = new LinkedList<>();
 
+    public static boolean input() {
+        Scanner in = new Scanner(System.in);
+        String text = "";
+        System.out.print("Start ticket time (default 480): ");
+        text = in.nextLine();
+        if (!text.equals("")) {
+            try {
+                START_TICKET_TIME = Integer.parseInt(text);
+                time = START_TICKET_TIME;
+            } catch (Exception ignored) {
+                return false;
+            }
+        } else {
+            START_TICKET_TIME = 480;
+            time = 480;
+        }
+        System.out.print("Start time (default 540): ");
+        text = in.nextLine();
+        if (!text.equals("")) {
+            try {
+                START_TIME = Integer.parseInt(text);
+            } catch (Exception ignored) {
+                return false;
+            }
+        } else {
+            START_TIME = 540;
+        }
+        System.out.print("End ticket time (default 1020): ");
+        text = in.nextLine();
+        if (!text.equals("")) {
+            try {
+                END_TICKET_TIME = Integer.parseInt(text);
+            } catch (Exception ignored) {
+                return false;
+            }
+        } else {
+            END_TICKET_TIME = 1020;
+        }
+        System.out.print("End time (default 1080): ");
+        text = in.nextLine();
+        if (!text.equals("")) {
+            try {
+                END_TIME = Integer.parseInt(text);
+            } catch (Exception ignored) {
+                return false;
+            }
+        } else {
+            END_TIME = 1080;
+        }
+        // validate
+        if(START_TICKET_TIME > START_TIME) {
+            System.out.println("Start ticket time cannot be later than start time.");
+            return false;
+        }
+        if(START_TIME > END_TIME) {
+            System.out.println("Start time cannot be later than end time.");
+            return false;
+        }
+        if(END_TICKET_TIME > END_TIME) {
+            System.out.println("End ticket time cannot be later than end time.");
+            return false;
+        }
+        return true;
+    }
+
     public static String timeToString() {
-        String h = Integer.toString(time.get() / 60);
+        String h = Integer.toString(time / 60);
         if (h.length() == 1) h = "0" + h;
-        String m = Integer.toString(time.get() % 60);
+        String m = Integer.toString(time % 60);
         if (m.length() == 1) m = "0" + m;
         return h + m;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        boolean valid = input();
+        while(!valid) valid = input();
         // new ticketing system
         ticketingSystem = new TicketingSystem();
         visitSystem = new VisitSystem();
         exitSystem = new ExitSystem();
-        while (time.get() <= END_TIME) {
+
+        while (time <= END_TIME) {
+            // threads
+            Thread visitThread = new Thread(visitSystem);
+            Thread exitThread = new Thread(exitSystem);
             // buy tickets
             ticketingSystem.buy();
-            // visit museum
-            boolean canVisit = true;
-            while (canVisit) canVisit = visitSystem.visit();
-            // exit museum
-            exitSystem.exit();
+            visitThread.start();
+            exitThread.start();
             // move time
-            time.incrementAndGet();
+            time++;
+            // sleep
+            Thread.sleep(100);
         }
+
+        // report
+        System.out.println("Total tickets sold: " + ticketingSystem.totalTickets);
+        System.out.println("Total tickets visited: " + visitSystem.totalTickets);
+        System.out.println("Total tickets exit: " + ticketsLeft.size());
     }
 }
+
