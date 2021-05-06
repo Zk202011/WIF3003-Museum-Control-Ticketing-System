81598fcd1a41d4f12c3e48cb3761833c9e7e99cb
Xuefeng-Qiao <74858375+Xuefeng-Qiao@users.noreply.github.com>
2021-05-07 00:26:23 +0800

    Ticket.java

---
+++ Ticket.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Ticket {
+    public String id;
+    public int time;
+    public int duration;
+    public int exitTime;
+
+    public Ticket(String id, int time) {
+        this.id = id;
+        this.time = time;
+    }
+}
