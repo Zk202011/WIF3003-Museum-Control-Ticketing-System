7e7fea5f1658ff12aa7249cc6b5bb46ff29cc393
alielborady <51218450+alielborady@users.noreply.github.com>
2021-05-07 15:49:04 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,6 +10,8 @@ public class Main {
     public static volatile int START_TIME = 540;
     public static volatile int END_TICKET_TIME = 1020;
     public static volatile int END_TIME = 1080;
+    public static volatile int MAX_HOURLY_LIMIT = 100;
+    public static volatile int MAX_DAILY_LIMIT = 900;
 
     static volatile int time = START_TICKET_TIME;
     static TicketingSystem ticketingSystem;
@@ -22,10 +24,11 @@ public class Main {
     public static boolean input() {
         Scanner in = new Scanner(System.in);
         String text = "";
-        System.out.print("Start ticket time (default 480): ");
+        System.out.print("Start ticket time (default 8:00): ");
         text = in.nextLine();
         if (!text.equals("")) {
             try {
+                text=Integer.parseInt(text)*60+"";
                 START_TICKET_TIME = Integer.parseInt(text);
                 time = START_TICKET_TIME;
             } catch (Exception ignored) {
@@ -35,10 +38,11 @@ public class Main {
             START_TICKET_TIME = 480;
             time = 480;
         }
-        System.out.print("Start time (default 540): ");
+        System.out.print("Start time (default 9:00): ");
         text = in.nextLine();
         if (!text.equals("")) {
             try {
+                text=Integer.parseInt(text)*60+"";
                 START_TIME = Integer.parseInt(text);
             } catch (Exception ignored) {
                 return false;
@@ -46,10 +50,11 @@ public class Main {
         } else {
             START_TIME = 540;
         }
-        System.out.print("End ticket time (default 1020): ");
+        System.out.print("End ticket time (default 17:00): ");
         text = in.nextLine();
         if (!text.equals("")) {
             try {
+                text=Integer.parseInt(text)*60+"";
                 END_TICKET_TIME = Integer.parseInt(text);
             } catch (Exception ignored) {
                 return false;
@@ -57,10 +62,11 @@ public class Main {
         } else {
             END_TICKET_TIME = 1020;
         }
-        System.out.print("End time (default 1080): ");
+        System.out.print("End time (default 18:00): ");
         text = in.nextLine();
         if (!text.equals("")) {
             try {
+                text=Integer.parseInt(text)*60+"";
                 END_TIME = Integer.parseInt(text);
             } catch (Exception ignored) {
                 return false;
@@ -69,17 +75,39 @@ public class Main {
             END_TIME = 1080;
         }
         // validate
-        if(START_TICKET_TIME > START_TIME) {
+        if (START_TICKET_TIME > START_TIME) {
             System.out.println("Start ticket time cannot be later than start time.");
             return false;
         }
-        if(START_TIME > END_TIME) {
+        if (START_TIME > END_TIME) {
             System.out.println("Start time cannot be later than end time.");
             return false;
         }
-        if(END_TICKET_TIME > END_TIME) {
+        if (END_TICKET_TIME > END_TIME) {
             System.out.println("End ticket time cannot be later than end time.");
             return false;
+        }
+        System.out.print("Max daily limit (default 900): ");
+        text = in.nextLine();
+        if (!text.equals("")) {
+            try {
+                MAX_DAILY_LIMIT = Integer.parseInt(text);
+            } catch (Exception ignored) {
+                return false;
+            }
+        } else {
+            MAX_DAILY_LIMIT = 900;
+        }
+        System.out.print("Max hourly limit (default 100): ");
+        text = in.nextLine();
+        if (!text.equals("")) {
+            try {
+                MAX_HOURLY_LIMIT = Integer.parseInt(text);
+            } catch (Exception ignored) {
+                return false;
+            }
+        } else {
+            MAX_HOURLY_LIMIT = 900;
         }
         return true;
     }
@@ -94,7 +122,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         boolean valid = input();
-        while(!valid) valid = input();
+        while (!valid) valid = input();
         // new ticketing system
         ticketingSystem = new TicketingSystem();
         visitSystem = new VisitSystem();
@@ -108,10 +136,10 @@ public class Main {
             ticketingSystem.buy();
             visitThread.start();
             exitThread.start();
-            // move time
-            time++;
             // sleep
             Thread.sleep(100);
+            // move time
+            time++;
         }
 
         // report
@@ -120,4 +148,3 @@ public class Main {
         System.out.println("Total tickets exit: " + ticketsLeft.size());
     }
 }
-
