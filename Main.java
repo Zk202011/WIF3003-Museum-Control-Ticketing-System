130ea0f0f5d843a0fec6261321dfe819e68fc6ea
wif170725 <74998926+wif170725@users.noreply.github.com>
2021-06-17 22:37:33 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 package main;
 
+import javax.swing.*;
 import java.util.LinkedList;
-import java.util.Scanner;
 import java.util.concurrent.PriorityBlockingQueue;
 
 public class Main {
@@ -20,131 +20,44 @@ public class Main {
     static LinkedList<Ticket> tickets = new LinkedList<>();
     static PriorityBlockingQueue<Ticket> ticketsEntered = new PriorityBlockingQueue<>();
     static LinkedList<Ticket> ticketsLeft = new LinkedList<>();
-
-    public static boolean input() {
-        Scanner in = new Scanner(System.in);
-        String text = "";
-        System.out.print("Start ticket time (default 8:00): ");
-        text = in.nextLine();
-        if (!text.equals("")) {
-            try {
-                text=Integer.parseInt(text)*60+"";
-                START_TICKET_TIME = Integer.parseInt(text);
-                time = START_TICKET_TIME;
-            } catch (Exception ignored) {
-                return false;
-            }
-        } else {
-            START_TICKET_TIME = 480;
-            time = 480;
-        }
-        System.out.print("Start time (default 9:00): ");
-        text = in.nextLine();
-        if (!text.equals("")) {
-            try {
-                text=Integer.parseInt(text)*60+"";
-                START_TIME = Integer.parseInt(text);
-            } catch (Exception ignored) {
-                return false;
-            }
-        } else {
-            START_TIME = 540;
-        }
-        System.out.print("End ticket time (default 17:00): ");
-        text = in.nextLine();
-        if (!text.equals("")) {
-            try {
-                text=Integer.parseInt(text)*60+"";
-                END_TICKET_TIME = Integer.parseInt(text);
-            } catch (Exception ignored) {
-                return false;
-            }
-        } else {
-            END_TICKET_TIME = 1020;
-        }
-        System.out.print("End time (default 18:00): ");
-        text = in.nextLine();
-        if (!text.equals("")) {
-            try {
-                text=Integer.parseInt(text)*60+"";
-                END_TIME = Integer.parseInt(text);
-            } catch (Exception ignored) {
-                return false;
-            }
-        } else {
-            END_TIME = 1080;
-        }
-        // validate
-        if (START_TICKET_TIME > START_TIME) {
-            System.out.println("Start ticket time cannot be later than start time.");
-            return false;
-        }
-        if (START_TIME > END_TIME) {
-            System.out.println("Start time cannot be later than end time.");
-            return false;
-        }
-        if (END_TICKET_TIME > END_TIME) {
-            System.out.println("End ticket time cannot be later than end time.");
-            return false;
-        }
-        System.out.print("Max daily limit (default 900): ");
-        text = in.nextLine();
-        if (!text.equals("")) {
-            try {
-                MAX_DAILY_LIMIT = Integer.parseInt(text);
-            } catch (Exception ignored) {
-                return false;
-            }
-        } else {
-            MAX_DAILY_LIMIT = 900;
-        }
-        System.out.print("Max hourly limit (default 100): ");
-        text = in.nextLine();
-        if (!text.equals("")) {
-            try {
-                MAX_HOURLY_LIMIT = Integer.parseInt(text);
-            } catch (Exception ignored) {
-                return false;
-            }
-        } else {
-            MAX_HOURLY_LIMIT = 900;
-        }
-        return true;
-    }
+    public static InputPanel inputPanel;
+    public static GamePanel gamePanel;
+    public static GraphicPanel graphicPanel;
+    public static JFrame mainFrame;
 
     public static String timeToString() {
         String h = Integer.toString(time / 60);
         if (h.length() == 1) h = "0" + h;
         String m = Integer.toString(time % 60);
         if (m.length() == 1) m = "0" + m;
-        return h + m;
+        return h + ":" + m;
+    }
+
+    public static void changePanel(JPanel panel) {
+        mainFrame.getContentPane().removeAll();
+        mainFrame.add(panel);
+        mainFrame.revalidate();
+        mainFrame.repaint();
     }
 
     public static void main(String[] args) throws InterruptedException {
-        boolean valid = input();
-        while (!valid) valid = input();
         // new ticketing system
         ticketingSystem = new TicketingSystem();
         visitSystem = new VisitSystem();
         exitSystem = new ExitSystem();
 
-        while (time <= END_TIME) {
-            // threads
-            Thread visitThread = new Thread(visitSystem);
-            Thread exitThread = new Thread(exitSystem);
-            // buy tickets
-            ticketingSystem.buy();
-            visitThread.start();
-            exitThread.start();
-            // sleep
-            Thread.sleep(100);
-            // move time
-            time++;
-        }
+        // initialize panels
+        inputPanel = new InputPanel();
+        gamePanel = new GamePanel();
+        graphicPanel = new GraphicPanel();
 
-        // report
-        System.out.println("Total tickets sold: " + ticketingSystem.totalTickets);
-        System.out.println("Total tickets visited: " + visitSystem.totalTickets);
-        System.out.println("Total tickets exit: " + ticketsLeft.size());
+        // configure frame
+        mainFrame = new JFrame("Main Menu");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(800, 600);
+        mainFrame.setResizable(false);
+        mainFrame.add(inputPanel);
+        mainFrame.setVisible(true);
+        mainFrame.setLocationRelativeTo(null);
     }
 }
